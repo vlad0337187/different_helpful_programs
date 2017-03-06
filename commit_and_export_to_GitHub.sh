87ba33961620570abf6437c0478245c22d0826bc
vlad1777d <naumovvladislav@mail.ru>
2017-03-06 06:33:42 +0200

    Changed export script.

--- commit_and_export_to_GitHub.sh
+++ commit_and_export_to_GitHub.sh
@@ -4,4 +4,4 @@ cd '/home/vlad/Programs/my_projects/different_helpful_programs'
 hg status
 hg commit
 hg bookmark -r default master
-hg push git+ssh://github.com/vlad1777d/different_helpful_programs.git
+hg push git+ssh://git@github.com/vlad1777d/different_helpful_programs.git
